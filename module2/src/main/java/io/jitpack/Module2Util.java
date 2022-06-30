d2ae397bffd495ae9c631abc2e8f7dbbf6fc3ae9
Anoohhya Choudavaram (FINDIA.DEL) <anoohhya.c@fsoft.com.vn>
2022-06-30 16:57:00 +0530

    modification

---
+++ module2/src/main/java/io/jitpack/Module2Util.java
@@ -0,0 +1,7 @@
+package io.jitpack;
+
+public class Module2Util{
+	public String testMethod1() {
+		return ("Hello Apple");
+	}
+}
---
+++ module3/src/main/java/com/fptSotware/Main.java
@@ -0,0 +1,19 @@
+package com.fptSotware;
+
+import io.jitpack.Module1Util;
+import io.jitpack.Module2Util;
+
+public class Main {
+
+	
+	public static void main(String args[]) 
+	{
+		Module1Util module=new Module1Util();
+		String result=module.testMethod();
+		System.out.println(result);
+		
+		Module2Util module1=new Module2Util();
+		String result1=module1.testMethod1();
+		System.out.println(result1);
+	}
+}
